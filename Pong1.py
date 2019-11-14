06868013e2030586a639cc9bcfa85b22180913c7
Munaiz <munaizahmed2@gmail.com>
2019-11-14 07:47:11 -0600

    Initial Commit + imported turtle in pycharm and getting first basic window

---
+++ Pong1.py
@@ -0,0 +1,11 @@
+import turtle
+
+window = turtle.Screen()
+window.title('Pong by @amunaizing')
+window.bgcolor('black')
+window.setup(width=700, height=500)
+window.tracer(0)
+
+# MAIN GAME LOOP
+while True:
+    window.update()
