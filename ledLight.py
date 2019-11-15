897f0c9d9292c68363a45add49c583c7dc38614b
Munaiz123 <munaizahmed2@gmail.com>
2019-11-15 17:02:22 -0600

    added GPIO light component

---
+++ ledLight.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+def flicker():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(18, GPIO.OUT)
+	GPIO.output(18, GPIO.HIGH)
+	time.sleep(.1)
+	GPIO.output(18, GPIO.LOW)
+
+
+	GPIO.cleanup
+
+flicker()
